a4b819d19e06e93da5a698ff467fc3a760fe8816
nikdimentiy <nikdimentiy@yahoo.com>
2021-04-22 18:40:22 -0700

    the code represent (with CLA) is Leap entered year or no

---
+++ LeapYear.java
@@ -0,0 +1,13 @@
+// the code represents (with Command Line Arguments) is Leap entered year or no
+
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = Integer.parseInt(args[0]);
+        boolean isLeapYear;
+        isLeapYear = (year % 4 == 0);
+        isLeapYear = isLeapYear && (year % 100 != 0);
+        isLeapYear = isLeapYear || (year % 400 == 0);
+        System.out.println("Entered year is LEAP: ");
+        System.out.println(isLeapYear);
+    }
+}
