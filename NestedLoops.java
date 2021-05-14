7bb071cd1bdf6e90d487637c213d6448dc16476e
nikdimentiy <nikdimentiy@yahoo.com>
2021-05-13 23:24:36 -0700

    the tiny code represent nested loop

---
+++ NestedLoops.java
@@ -0,0 +1,12 @@
+// the code represent nested loop 
+public class NestedLoops {
+
+    public static void main(String[] args) {
+        for (int k = 1; k <= 6; k++) {
+            for (int i = 1; i <= 6; i++) {
+                System.out.print("$" + " ");
+            }
+            System.out.println();
+        }
+    }
+}
