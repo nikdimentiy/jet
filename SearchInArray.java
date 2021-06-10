6082f3612eb6c8a05d9f4aa35b95aa3fe02bd999
nikdimentiy <nikdimentiy@yahoo.com>
2021-06-09 23:05:07 -0700

    Create SearchInArray.java
    search in the array using an extended loop (for) -> (for-each)

---
+++ SearchInArray.java
@@ -0,0 +1,18 @@
+class SearchInArray {
+    public static void main(String[] args) {
+        int nums[] = {6, 8, 3, 7, 5, 6, 1, 4};
+        int val = 5;
+        boolean found = false;
+
+        //using cycle (for-each) to find the value of a variable (val) in the array (nums)
+        for (int x : nums) {
+            if (x == val) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            System.out.println("The value is found!");
+        }
+    }
+}
