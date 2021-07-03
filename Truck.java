32e10e0440c7e6b824d6002ea247b1efb4c2ee15
nikdimentiy <nikdimentiy@yahoo.com>
2021-07-02 23:23:36 -0700

    Create Truck.java
    night coding....checking a truck weight...tiny code

---
+++ Truck.java
@@ -0,0 +1,46 @@
+public class Truck {
+	int length;
+	int width;
+	int height;
+	int weight;
+
+	public int getVolume() {
+		int volume = length * width * height;
+		return volume;
+	}
+
+	public static void main(String[] args) {
+		Truck first = new Truck();
+		first.weight = 10000;
+		Truck second = new Truck();
+		second.weight = 20000;
+
+		BridgeOfficer officer = new BridgeOfficer(15000);
+		System.out.println("Truck #1. It's possible to go ahead?");
+		boolean canFirstTruckGo = officer.checkTruck(first);
+		System.out.println(canFirstTruckGo);
+
+		System.out.println();
+
+		System.out.println("Truck #2. It's possible to go ahead?");
+		boolean canSecondTruckGo = officer.checkTruck(second);
+		System.out.println(canSecondTruckGo);
+	}
+
+}
+
+class BridgeOfficer {
+	int maxWeight;
+
+	public BridgeOfficer(int normalWeight) {
+		this.maxWeight = normalWeight;
+	}
+
+	public boolean checkTruck(Truck truck) {
+		if (truck.weight > maxWeight) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+}
