2f90a7e7a6788991413aba553e763ffdef4243b2
nikdimentiy <nikdimentiy@yahoo.com>
2021-07-15 20:21:05 -0700

    Update InsertionSortExample.java
    added comments of insert sort (Wiki)

--- InsertionSortExample.java
+++ InsertionSortExample.java
@@ -1,3 +1,9 @@
+/* Insertion sort is a simple sorting algorithm that 
+   builds the final sorted array one item at a time. 
+   It is much less efficient on large lists than more advanced algorithms
+   such as quicksort, heapsort, or merge sort.
+*/
+
 public class InsertionSortExample {
 
     public static void main(String a[]) {
