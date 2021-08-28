c2f38cf5a186f9dfdb1ff745c1b69dcb288c7797
nikdimentiy <nikdimentiy@yahoo.com>
2021-08-27 22:27:59 -0700

    Create TestEnumerated.java
    Test Enumerated method for Enum

---
+++ TestEnumerated.java
@@ -0,0 +1,20 @@
+enum Company {
+    // Instances
+    WEBCAMP(1000), GOOGLE(100), SKYNET(10); 
+    int value;
+
+    // The constructor can only be private
+    Company(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
+
+public class TestEnumerated {
+    public static void main(String[] args) {
+        Company myComp = Company.WEBCAMP;
+        System.out.println("I work in a company " + myComp + " and I earn " + myComp.getValue() + "$ per month.");
+  
