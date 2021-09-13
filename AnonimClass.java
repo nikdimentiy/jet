36b0692dc7212fbf94ef84851d8618314a92d7e4
nikdimentiy <nikdimentiy@yahoo.com>
2021-09-12 22:07:18 -0700

    Create AnonimClass.java

---
+++ AnonimClass.java
@@ -0,0 +1,49 @@
+package practice;
+
+/**
+ * Anonymous classes
+ */
+interface Interface {
+    void method();
+
+    int getValue();
+}
+
+public class AnonimClass {
+    public static void main(String[] args) {
+        // Anonymous (inner) class declaration
+        Interface instance1 = new Interface() {
+            int field = 0;
+
+            @Override
+            public void method() {
+                field = 100;
+            }
+
+            @Override
+            public int getValue() {
+                return field;
+            }
+        };
+
+        Interface instance2 = new Interface() {
+            int field = -1;
+
+            @Override
+            public void method() {
+                field = instance1.getValue();
+            }
+
+            @Override
+            public int getValue() {
+                return field;
+            }
+        };
+
+        instance2.method();
+        instance1.method();
+
+        System.out.println("instance1: field = " + instance1.getValue() + "    instance2: field = " + instance2.getValue());
+    }
+}
+
