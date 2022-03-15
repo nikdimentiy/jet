f38c0d98806d62a7fbcd04baca58493e8eeda340
nikdimentiy <nikdimentiy@yahoo.com>
2022-03-14 23:11:54 -0700

    Create AbstractBaseClass.java

---
+++ Abstract_class/AbstractBaseClass.java
@@ -0,0 +1,13 @@
+
+public abstract class AbstractBaseClass {
+
+    public void simpleMethod() {
+        System.out.println("AbstractBaseClass.simpleMethod");
+    }
+
+    public void overriddenMethod() {
+        System.out.println("AbstractBaseClass.overriddenMethod");
+    }
+
+    abstract public void abstractMethod();
+}
