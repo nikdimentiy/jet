2b74998f6e15dc3202c869a8a6d5dd03d755e6b3
nikdimentiy <nikdimentiy@yahoo.com>
2022-03-14 23:10:27 -0700

    Create main.java

---
+++ Abstract_class/main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        ConcreteDerivedClass instance = new ConcreteDerivedClass();
+
+        instance.simpleMethod();
+        instance.overriddenMethod();
+        instance.abstractMethod();
+    }
+}
