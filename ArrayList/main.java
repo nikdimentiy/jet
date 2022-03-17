7e898320515bd3b04b4e80864dfe5f6b415e224d
nikdimentiy <nikdimentiy@yahoo.com>
2022-03-16 22:24:26 -0700

    Update main.java

--- ArrayList/main.java
+++ ArrayList/main.java
@@ -3,23 +3,23 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-    
+        // List declaration
         ArrayList<String> arrayList = new ArrayList<>();
 
 
         arrayList.add("Hello");
         arrayList.add("student");
         arrayList.add("would you?");
-
+        // adding by index
         System.out.println(arrayList);
 
-
+        // adding elements by index
         arrayList.add(0, "teacher");
 
 
         arrayList.remove(1);
 
-
+        // remove (delete) elements by value
         arrayList.remove("would you?");
 
         System.out.println(arrayList);
