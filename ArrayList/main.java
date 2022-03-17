fd7918a161e5e41542abd7d2dfff673f1609b698
nikdimentiy <nikdimentiy@yahoo.com>
2022-03-16 22:21:29 -0700

    Create main.java

---
+++ ArrayList/main.java
@@ -0,0 +1,27 @@
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        ArrayList<String> arrayList = new ArrayList<>();
+
+
+        arrayList.add("Hello");
+        arrayList.add("student");
+        arrayList.add("would you?");
+
+        System.out.println(arrayList);
+
+
+        arrayList.add(0, "teacher");
+
+
+        arrayList.remove(1);
+
+
+        arrayList.remove("would you?");
+
+        System.out.println(arrayList);
+    }
+}
