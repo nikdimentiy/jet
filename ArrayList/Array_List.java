14453027ae8304912d90b6542afe2ade2f7247b1
nikdimentiy <nikdimentiy@yahoo.com>
2022-03-17 22:22:13 -0700

    Update and rename main.java to Array_List.java

--- ArrayList/Array_List.java
+++ ArrayList/Array_List.java
@@ -1,7 +1,7 @@
 
 import java.util.ArrayList;
 
-public class Main {
+public class Array_List {
     public static void main(String[] args) {
         // List declaration
         ArrayList<String> arrayList = new ArrayList<>();
