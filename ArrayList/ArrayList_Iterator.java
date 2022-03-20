ae224db2cd1fa243c590a186ec45613650d1d96e
nikdimentiy <nikdimentiy@yahoo.com>
2022-03-19 23:16:23 -0700

    Update ArrayList_Iterator.java

--- ArrayList/ArrayList_Iterator.java
+++ ArrayList/ArrayList_Iterator.java
@@ -9,13 +9,15 @@ public class ArrayList_Iterator {
         arrayList.add(3);
         arrayList.add(5);
         arrayList.add(7);
-
+        // with the help of an iterator we can run through our list
         Iterator<Integer> iterator = arrayList.iterator();
-
+        // ask if there is the next element in the list, if true, then the while body will be implemented
         while (iterator.hasNext()) {
+            // move to the next element in the list and display it
             Integer integer = iterator.next();
             System.out.println(integer);
         }
+        // the same action, but through an optimized iterator for lists
         ListIterator<Integer> listIterator = arrayList.listIterator();
 
         while (listIterator.hasNext()) {
