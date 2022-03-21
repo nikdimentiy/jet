43a2f406f56e5fff8bdeeec20d39cffcb0c41c9f
nikdimentiy <nikdimentiy@yahoo.com>
2022-03-20 22:59:32 -0700

    Update ArrayList_Index.java

--- ArrayList/ArrayList_Index.java
+++ ArrayList/ArrayList_Index.java
@@ -1,23 +1,27 @@
 
 import java.util.ArrayList;
 
-public class ArrayList_index {
+public class ArrayList_Index {
 
     public static void main(String[] args) {
         ArrayList<String> arrayList = new ArrayList<>();
-
+        
         arrayList.add("name");
         arrayList.add("surname");
-
+        
+        // define capacity of List
         arrayList.ensureCapacity(30);
-
+        
+        // print List in this state
         System.out.println(arrayList.size());
-
+        
+        // get element by index
         System.out.println(arrayList.get(1));
 
-
+        // IndexOutOfBoundException error 
         // System.out.println(arrayList.get(2));
-
+        
+        // get the index of the object 
         System.out.println(arrayList.indexOf("name"));
 
         System.out.println(arrayList.lastIndexOf("surname"));
