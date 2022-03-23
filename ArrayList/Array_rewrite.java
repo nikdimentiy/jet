094cdfe0958543a8a405d96dd9b5f83be30e9b1d
nikdimentiy <nikdimentiy@yahoo.com>
2022-03-22 22:45:08 -0700

    Update Array_rewrite.java

--- ArrayList/Array_rewrite.java
+++ ArrayList/Array_rewrite.java
@@ -3,8 +3,12 @@ public class Array_rewrite {
         // создание массива размерностью 5
         int[] arr1 = new int[5];
         System.out.println(arr1.length);
-
-
+        
+        // creation a new array
+        
+        /* That is, we have created a new object - array (through "new"),
+        and there are no longer references to the previous object.*/
+        
         arr1 = new int[25];
         System.out.println(arr1.length);
     }
