9e9317a5c8f5a5690ed0b02c2af5a773a91ef155
nikdimentiy <nikdimentiy@yahoo.com>
2022-03-22 22:41:37 -0700

    Create Array_rewrite.java

---
+++ ArrayList/Array_rewrite.java
@@ -0,0 +1,11 @@
+public class Array_rewrite {
+    public static void main(String[] args) {
+        // создание массива размерностью 5
+        int[] arr1 = new int[5];
+        System.out.println(arr1.length);
+
+
+        arr1 = new int[25];
+        System.out.println(arr1.length);
+    }
+}
