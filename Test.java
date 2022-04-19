8db1d64d5fbbc377e38e9a2139daa088191624ab
nikdimentiy <nikdimentiy@yahoo.com>
2022-04-18 23:26:26 -0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
 class Test {
-	static void sum(int... a) {
+	static void sum(int... a) { // test of vararg - it's the array of arguments
 		int sum = 0;
 		for (int i = 0; i < a.length; i++) {
 			sum += a[i];
@@ -8,6 +8,6 @@ class Test {
 	}
 
 	public static void main(String[] args) {
-		sum(1, 8, 15);
+		sum(1, 8, 15); // equals sum(new int[]{1, 8, 15})
 	}
 }
