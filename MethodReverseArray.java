20be33cb1f253b33ba2264701f316ff3e21e1e80
nikdimentiy <nikdimentiy@yahoo.com>
2022-07-13 14:30:33 -0700

    Add files via upload

---
+++ MethodReverseArray.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class MethodReverseArray {
+
+	public static void main(String[] args) {
+		var arr_1 = new int[5];
+		arr_1 = new int[] { 3, 5, 1, 9, 7 };
+		Arrays.sort(arr_1);
+		reverse(arr_1);
+		System.out.println(Arrays.toString(arr_1));
+	}
+
+	private static void reverse(int[] arr_1) {
+		int n = arr_1.length;
+
+		for (int i = 0; i < n / 2; i++) {
+			int temp = arr_1[i];
+			arr_1[i] = arr_1[n - i - 1];
+			arr_1[n - i - 1] = temp;
+		}
+
+	}
+}
