f881d78538e2a49994369d81931685f875044dc4
nikdimentiy <nikdimentiy@yahoo.com>
2022-07-28 22:01:26 -0700

    Add files via upload

---
+++ super_contstructor.java
@@ -0,0 +1,16 @@
+class A {
+    public A() {
+        System.out.println("New A");
+    }
+}
+class B extends A {
+    public B() {
+        System.out.println("New B");
+    }
+}
+
+class Program {
+    public static void main(String[ ] args) {
+        B obj = new B();
+    }
+}
