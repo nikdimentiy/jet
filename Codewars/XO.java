31262b02b2ea449aa6ea93b52b3853f056e9b176
nikdimentiy <nikdimentiy@yahoo.com>
2022-09-28 22:10:13 -0700

    Update XO.java

--- Codewars/XO.java
+++ Codewars/XO.java
@@ -1,39 +1,44 @@
+// Check to see if a string has the same amount of 'x's and 'o's.
+// The method must return a boolean and be case insensitive.
+// The string can contain any char.
+
+
 public class XO {
-    public static boolean getXO (String str) {
-        boolean flag=false;
-        int o_number=0;
-        int x_number=0;
-        if((str.contains("o")||str.contains("O"))&&(str.contains("x")||str.contains("X"))){
-            for(int i=0;i<str.length();i++){
-                String o=str.charAt(i)+"";
-                if(o.trim().equals("o")||o.trim().equals("O")){
+    public static boolean getXO(String str) {
+        boolean flag = false;
+        int o_number = 0;
+        int x_number = 0;
+        if ((str.contains("o") || str.contains("O")) && (str.contains("x") || str.contains("X"))) {
+            for (int i = 0; i < str.length(); i++) {
+                String o = str.charAt(i) + "";
+                if (o.trim().equals("o") || o.trim().equals("O")) {
                     o_number++;
                 }
             }
-            for(int i=0;i<str.length();i++){
-                String x=str.charAt(i)+"";
-                if(x.trim().equals("x")||x.trim().equals("X")){
+            for (int i = 0; i < str.length(); i++) {
+                String x = str.charAt(i) + "";
+                if (x.trim().equals("x") || x.trim().equals("X")) {
                     x_number++;
                 }
             }
-            if(o_number==x_number){
-                flag=true;
+            if (o_number == x_number) {
+                flag = true;
             }
-        }else {
-            flag=true;
+        } else {
+            flag = true;
         }
-        if((str.contains("o")||str.contains("O"))&&!(str.contains("x")||str.contains("X"))){
-            flag=false;
+        if ((str.contains("o") || str.contains("O")) && !(str.contains("x") || str.contains("X"))) {
+            flag = false;
         }
-        if(!(str.contains("o")||str.contains("O"))&&(str.contains("x")||str.contains("X"))){
-            flag=false;
+        if (!(str.contains("o") || str.contains("O")) && (str.contains("x") || str.contains("X"))) {
+            flag = false;
         }
         return flag;
     }
 
-    public static boolean getXO2 (String str) {
+    public static boolean getXO2(String str) {
         str = str.toLowerCase();
-        return str.replace("o","").length() == str.replace("x","").length();
+        return str.replace("o", "").length() == str.replace("x", "").length();
 
     }
 
