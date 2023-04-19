4ebbbe1923ea150377dbea75481d98cae5de8bfe
nikdimentiy <nikdimentiy@yahoo.com>
2023-04-18 20:26:29 -0700

    Update RockPaperScissors.java

--- Codewars/RockPaperScissors.java
+++ Codewars/RockPaperScissors.java
@@ -1,10 +1,32 @@
+package codewars
+
 public class RockPaperScissors {
+    /**
+     * Determines the winner of a rock-paper-scissors game.
+     *
+     * @param p1 the choice of player 1 ("rock", "paper", or "scissors")
+     * @param p2 the choice of player 2 ("rock", "paper", or "scissors")
+     * @return the result of the game: either "Draw!" if the choices are the same,
+     *         or "Player X won!", where X is the number of the winning player.
+     */
     public static String rps(String p1, String p2) {
-        if (p1 == p2)
+        if (p1.equals(p2)) {
             return "Draw!";
-        int p = (p1 + p2).equals("scissorspaper") || (p1 + p2).equals("rockscissors") || (p1 + p2).equals("paperrock")
-                ? 1
-                : 2;
-        return "Player " + p + " won!";
+        } else if ((p1 + p2).equals("scissorspaper") || (p1 + p2).equals("rockscissors") || (p1 + p2).equals("paperrock")) {
+            return "Player 1 won!";
+        } else {
+            return "Player 2 won!";
+        }
+    }
+
+    /**
+     * A simple driver code to test the rps() method.
+     */
+    public static void main(String[] args) {
+        String result1 = rps("rock", "scissors");
+        System.out.println(result1); // Output: "Player 1 won!"
+
+        String result2 = rps("paper", "paper");
+        System.out.println(result2); // Output: "Draw!"
     }
 }
