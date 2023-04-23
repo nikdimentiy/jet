fd7f5bff65a2055ff06a0a329e00b3ab63d6d60b
nikdimentiy <nikdimentiy@yahoo.com>
2023-04-22 21:46:35 -0700

    Update MaxArray.java
    add comments for each line of code

--- MaxArray.java
+++ MaxArray.java
@@ -1,18 +1,24 @@
 public class MaxArray {
+    /**
+     * This program finds the maximum element in an integer array.
+     * It prints the maximum value of the array to the console.
+     */
     public static void main(String[] args) {
         // creating an array of integer elements
-
         int[] list = {5, 6, 7, -7, -9, 2, 0, 11, -35, 5};
 
         // creating an integer variable "max" and entering a number in it,
         // which is contained in the null index of the array "list"
-
         int max = list[0];
 
+        // loop through the array starting from the second element
         for (int i = 1; i < list.length; i++) {
+            // if current element is greater than current maximum, update maximum
             if (list[i] > max)
                 max = list[i];
         }
+
+        // print the maximum value
         System.out.println("Max is " + max);
     }
 }
