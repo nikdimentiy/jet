32ef81c05e4772dd3e4a53f1d0d204cd57afc4d8
nikdimentiy <nikdimentiy@yahoo.com>
2023-04-24 20:15:54 -0700

    Create HelloWorld.java
    CodeWars kata --> solution

---
+++ Codewars/HelloWorld.java
@@ -0,0 +1,19 @@
+// CodeWars task: solution --> https://www.codewars.com/kata/584c7b1e2cb5e1a727000047/java
+
+public class HelloWorld {
+    public static String helloWorld() {
+        String H = new String(new char[] {72});
+        String e = new String(new char[] {101});
+        String l1 = new String(new char[] {108});
+        String l2 = new String(new char[] {108});
+        String o = new String(new char[] {111});
+        String comma = new String(new char[] {44});
+        String space = new String(new char[] {32});
+        String W = new String(new char[] {87});
+        String r = new String(new char[] {114});
+        String d = new String(new char[] {100});
+        String exclamation = new String(new char[] {33});
+
+        return H + e + l1 + l2 + o + comma + space + W + o + r + l1 + d + exclamation;
+    }
+}
