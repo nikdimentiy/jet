fe1b89f4e633c513c0f0269410396c8d1a24237a
nikdimentiy <nikdimentiy@yahoo.com>
2023-04-26 22:10:01 -0700

    Update BinarySearch.java
    add comments

--- BinarySearch.java
+++ BinarySearch.java
@@ -2,27 +2,47 @@ import java.util.Arrays;
 
 public class BinarySearch {
 
+    /**
+     * This class should not be instantiated.
+     */
     private BinarySearch() { }
 
+    /**
+     * Returns the index of the specified key in the specified array.
+     *
+     * @param a the array of integers, must be sorted in ascending order
+     * @param key the search key
+     * @return the index of the key in the array, or -1 if the key is not found
+     */
     public static int indexOf(int[] a, int key) {
-        int lo = 0;
-        int hi = a.length - 1;
-        while (lo <= hi) {
-            // Key is in a[lo..hi] or not present.
-            int mid = lo + (hi - lo) / 2;
-            if      (key < a[mid]) hi = mid - 1;
-            else if (key > a[mid]) lo = mid + 1;
-            else return mid;
+        int lo = 0; // lower bound of the search range
+        int hi = a.length - 1; // upper bound of the search range
+        while (lo <= hi) { // continue searching while the search range is non-empty
+            int mid = lo + (hi - lo) / 2; // find the middle index of the search range
+            if      (key < a[mid]) hi = mid - 1; // the key is in the left half of the search range
+            else if (key > a[mid]) lo = mid + 1; // the key is in the right half of the search range
+            else return mid; // the key is found at the middle index
         }
-        return -1;
+        return -1; // the key is not found in the array
     }
 
-
+    /**
+     * Returns the index of the specified key in the specified array.
+     *
+     * @param key the search key
+     * @param a the array of integers, must be sorted in ascending order
+     * @return the index of the key in the array, or -1 if the key is not found
+     */
     public static int rank(int key, int[] a) {
         return indexOf(a, key);
     }
 
-
+    /**
+     * Reads a sequence of integers from a file, sorts them, and prints any integer
+     * that is not in the whitelist (stdin by default).
+     *
+     * @param args the command-line arguments
+     */
     public static void main(String[] args) {
 
         // read the integers from a file
