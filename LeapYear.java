4dd1b8ff93a5a714fd72a3cfea73ab05c481c56f
nikdimentiy <nikdimentiy@yahoo.com>
2023-04-27 12:24:30 -0700

    Update LeapYear.java
    Change code structure and add comments

--- LeapYear.java
+++ LeapYear.java
@@ -1,13 +1,57 @@
-// the code represents (with Command Line Arguments) is Leap entered year or no
-
+import java.util.Scanner;
+/**
+ * This class determines whether a year is a leap year.
+ *
+ * @author Dmitriy Nikey
+ */
 public class LeapYear {
+
+    /**
+     * This method prompts the user to enter a year and prints whether it is a leap year or not.
+     *
+     * @param args The year to check.
+     */
     public static void main(String[] args) {
-        int year = Integer.parseInt(args[0]);
-        boolean isLeapYear;
-        isLeapYear = (year % 4 == 0);
-        isLeapYear = isLeapYear && (year % 100 != 0);
-        isLeapYear = isLeapYear || (year % 400 == 0);
-        System.out.println("Entered year is LEAP: ");
-        System.out.println(isLeapYear);
+
+        // Create a Scanner object to read user input.
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter a year.
+        System.out.println("Enter a year: ");
+        int year = scanner.nextInt();
+
+        // Check if the year is a leap year.
+        boolean isLeapYear = isLeapYear(year);
+
+        // Print whether the year is a leap year or not.
+        System.out.println("Is " + year + " a leap year? " + isLeapYear);
+    }
+
+    /**
+     * This method determines whether a year is a leap year.
+     *
+     * @param year The year to check.
+     * @return true if the year is a leap year, false otherwise.
+     */
+    public static boolean isLeapYear(int year) {
+
+        // Check if the year is a multiple of 4.
+        if (year % 4 == 0) {
+
+            // If the year is a multiple of 4, check if it is a multiple of 100.
+            if (year % 100 == 0) {
+
+                // If the year is a multiple of 100, check if it is a multiple of 400.
+                if (year % 400 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
     }
 }
