4db6d0e655cfbe108c0822f7ef70bf244d47f2be
nikdimentiy <nikdimentiy@yahoo.com>
2023-04-28 22:41:54 +0000

    Renaming class and add comments

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,19 +1,33 @@
 import java.util.Arrays;
 
-// java practice
-public class Bubble {
+/**
+ * Bubble class for sorting an array using Bubble sort algorithm
+ */
+
+public class BubbleSort {
+
+    /**
+     * Main method to execute the sorting
+     *
+     * @param args command line arguments (not used here)
+     */
+
     public static void main(String[] args) {
+
+        // Array to be sorted
         int[] nums = {99, -10, 100123, 18, -978, 5623, 463, -9, 287, 49};
 
+        // Declare variables
         int head, tail, temp;
-        // show entered array
+
+        // Display the source array
         System.out.print("Source array: ");
         for (int num : nums) {
             System.out.print(" " + num);
         }
         System.out.println("\n");
 
-        // bubble sort --> realisation
+        // Bubble Sort implementation
         for (head = 1; head < nums.length; head++) {
             for (tail = nums.length - 1; tail >= head; tail--) {
                 if (nums[tail - 1] > nums[tail]) {
@@ -23,10 +37,9 @@ public class Bubble {
                 }
             }
         }
-        /* show sorted array */
+
+        // Display the sorted array
         System.out.println("Sorted array: ");
-//        for (int num : nums) System.out.print(" " + num);
-        System.out.println();
         System.out.println(Arrays.toString(nums));
     }
 }
