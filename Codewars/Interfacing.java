0c3eef5eac46f486c3850384e115f4936e3864d1
nikdimentiy <nikdimentiy@yahoo.com>
2023-05-06 20:53:24 -0700

    Update Interfacing.java

--- Codewars/Interfacing.java
+++ Codewars/Interfacing.java
@@ -1,77 +1,61 @@
 // CodeWars task: solution -> https://www.codewars.com/kata/54c183dd3f069611c3000f97/train/java
 
-import java.util.*;
-import java.lang.reflect.*;
-
+/**
+ * A utility class that creates a dynamic proxy object based on the provided
+ * interface(s). The proxy object allows setting and getting field values 
+ * dynamically through generated getter and setter methods.
+ */
 public class Interfacing {
 
     /**
-     * Create an object that implements the given list of interfaces.
-     * Each interface declares a list of method pairs, each of which is made up of a get and a set method.
-     *
-     * @param interfaces an array of Class objects representing the interfaces to be implemented
-     * @return an object that implements all the given interfaces, or null if there is an error
+     * Creates a dynamic proxy object that implements the given interface(s). 
+     * The generated proxy object allows setting and getting field values 
+     * dynamically through generated getter and setter methods.
+     * 
+     * @param interfaces an array of interfaces to implement
+     * @return a proxy object that implements the given interface(s)
      */
     public static Object create(Class<?>[] interfaces) {
-        // Create an array to hold the InvocationHandler objects for each interface
-        InvocationHandler[] handlers = new InvocationHandler[interfaces.length];
-
-        // Create a new instance of the Proxy class for each interface
-        for (int i = 0; i < interfaces.length; i++) {
-            // Check if the interface is valid
-            if (!interfaces[i].isInterface()) {
-                System.err.println("Error: " + interfaces[i] + " is not an interface.");
-                return null;
-            }
-
-            // Create a new InvocationHandler for this interface
-            handlers[i] = new InterfaceHandler();
-
-            // Create a new instance of the Proxy class for this interface and its InvocationHandler
-            Object proxy = Proxy.newProxyInstance(
-                    interfaces[i].getClassLoader(),
-                    new Class[] { interfaces[i] },
-                    handlers[i]
-            );
-        }
-
-        // Combine all the interfaces into a single object that implements all of them
-        Object result = handlers[0].getObject();
-
-        // If there are more than one interfaces, combine the objects into a single object that implements all of them
-        for (int i = 1; i < handlers.length; i++) {
-            Object obj = handlers[i].getObject();
-            result = combineObjects(result, obj);
-        }
-
-        return result;
-    }
-
-    /**
-     * Combine two objects into a single object that implements all of their interfaces.
-     *
-     * @param obj1 the first object to be combined
-     * @param obj2 the second object to be combined
-     * @return a single object that implements all of the interfaces implemented by obj1 and obj2
-     */
-    private static Object combineObjects(Object obj1, Object obj2) {
-        // Get the list of interfaces implemented by each object
-        Class<?>[] interfaces1 = obj1.getClass().getInterfaces();
-        Class<?>[] interfaces2 = obj2.getClass().getInterfaces();
-
-        // Combine the two lists of interfaces into a single list
-        Class<?>[] combinedInterfaces = Arrays.copyOf(interfaces1, interfaces1.length + interfaces2.length);
-        System.arraycopy(interfaces2, 0, combinedInterfaces, interfaces1.length, interfaces2.length);
-
-        // Create a new InvocationHandler that can handle all of the methods from both objects
-        InvocationHandler handler = new CombinedHandler(obj1, obj2);
-
-        // Create a new instance of the Proxy class that implements all of the interfaces and uses the new InvocationHandler
         return Proxy.newProxyInstance(
-                obj1.getClass().getClassLoader(),
-                combinedInterfaces,
-                handler
+                ClassHelper.getClassLoader(),
+                interfaces,
+                new InvocationHandler() {
+                    
+                    // A table to store field values
+                    private Map<String, Object> fields = new HashMap<>();
+
+                    /**
+                     * Handles method invocations on the generated proxy object.
+                     * If the method starts with "get", it returns the value of the 
+                     * corresponding field. If the method starts with "set", it sets 
+                     * the value of the corresponding field to the provided argument. 
+                     * Throws UnsupportedOperationException if the method name does 
+                     * not start with "get" or "set".
+                     * 
+                     * @param instance the generated proxy object
+                     * @param method the method being invoked
+                     * @param args the arguments passed to the method
+                     * @return the value returned by the method
+                     * @throws UnsupportedOperationException if the method name does not start with "get" or "set"
+                     */
+                    @Override
+                    public Object invoke(Object instance, Method method, Object[] args) {
+                        if(method.getName().startsWith("get")) {
+                            // Getter method was called
+                            return fields.get(method.getName().substring(3));
+                        } else if(method.getName().startsWith("set")) {
+                            // Setter method was called
+                            fields.put(method.getName().substring(3), args[0]);
+                            return null;
+                        } else {
+                            // Something went horribly wrong
+                            throw new UnsupportedOperationException();
+                        }
+                    }
+                }
         );
     }
-
 }
+
+
+
