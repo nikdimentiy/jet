431636445ea5e065552796bd6309454d2dde6740
nikdimentiy <nikdimentiy@yahoo.com>
2023-09-29 22:35:30 -0700

    Update ACopy.java

--- ACopy.java
+++ ACopy.java
@@ -1,22 +1,21 @@
 // filling array and coping operation into another array
 
 class ACopy {
-    public static void main(String[] args) {
-        int i;
-        int nums1[] = new int[10];
-        int nums2[] = new int[10];
-
-        for (i = 0; i < nums1.length; i++) {
+    public void main(int[] nums1, int[] nums2) {
+        /**
+         * This function copies the elements of array nums1 to array nums2.
+         * @param nums1: array of integers
+         * @param nums2: array of integers
+         */
+        for (int i = 0; i < nums1.length; i++) {
             nums1[i] = i;
         }
-
         // Copy array nums1 to array nums2
         if (nums2.length >= nums1.length) {
-            for (i = 0; i < nums2.length; i++) {
+            for (int i = 0; i < nums2.length; i++) {
                 nums2[i] = nums1[i];
             }
-
-            for (i = 0; i < nums2.length; i++) {
+            for (int i = 0; i < nums2.length; i++) {
                 System.out.print(nums2[i] + " ");
             }
         }
