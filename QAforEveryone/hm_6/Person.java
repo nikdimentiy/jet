b2c74fccd0830bdbb51a25c633ed84d019563c08
nikdimentiy <nikdimentiy@yahoo.com>
2023-10-19 19:05:42 -0700

    Update Person.java

--- QAforEveryone/hm_6/Person.java
+++ QAforEveryone/hm_6/Person.java
@@ -1,39 +1,46 @@
-package hw_6;
+package hw_6; // This is the package declaration, specifying the package name for the class.
 
-import java.util.Objects;
+import java.util.Objects; // Importing the Objects class from the java.util package.
 
-public class Person {
-    private String name;
-    private int age;
-    private String gender;
+public class Person { // Defining the Person class.
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    private String name; // Private member variable to store the person's name.
+    private int age; // Private member variable to store the person's age.
+    private String gender; // Private member variable to store the person's gender.
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
+    // Constructor to initialize a Person object with a name, age, and gender.
     public Person(String name, int age, String gender) {
         this.name = name;
         this.age = age;
         this.gender = gender;
     }
 
+    // Setter method to set the person's name.
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    // Getter method to retrieve the person's age.
+    public int getAge() {
+        return age;
+    }
+
+    // Setter method to set the person's age.
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Getter method to retrieve the person's gender.
+    public String getGender() {
+        return gender;
+    }
+
+    // Setter method to set the person's gender.
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    // This method prints a greeting based on the person's gender and name.
     public void getName() {
         if (Objects.equals(gender, "M")) {
             System.out.println("Hello Mr. " + name);
@@ -42,4 +49,3 @@ public class Person {
         }
     }
 }
-
