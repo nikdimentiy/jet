c7802dd7bc331d2e5c3ccbaa8d1c716bc1fba06d
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-10-21 19:27:27 -0700

    Update ErrorFloatCalc.java

--- ErrorFloatCalc.java
+++ ErrorFloatCalc.java
@@ -1,14 +1,24 @@
-// for a visual representation of the error of float calculations
 public class ErrorFloatCalc {
     public static void main(String[] args) {
         double a;
+
+        // Perform a series of floating-point additions
         a = 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1;
 
+        // Print the result of the addition
         System.out.println("Result of operation addition is: " + a);
+
+        // Check if the result 'a' is equal to 1
         if (a == 1)
             System.out.println("The value of the result of an addition is equivalent to unity!");
         else
-            System.out.println("This is mistake!");
+            System.out.println("This is a mistake!");
+    }
 
+    // Driver code to execute the ErrorFloatCalc class
+    public static void main(String[] args) {
+        ErrorFloatCalc errorFloatCalc = new ErrorFloatCalc();
+        errorFloatCalc.main(args);
     }
 }
+
