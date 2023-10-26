38b3cbb9f70582977aad92144d96d7b43af3ba47
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-10-25 22:14:16 -0700

    Update Swallow.java

--- animal/Swallow.java
+++ animal/Swallow.java
@@ -1,8 +1,15 @@
+/**
+ * The Swallow class is a subclass of the Bird class and represents a specific type of bird, the swallow.
+ * Swallows are known for their flying abilities.
+ */
 package animal;
 
-public class Swallow extends Bird{
+public class Swallow extends Bird {
+    /**
+     * Overrides the move() method to specify the Swallow's way of moving, which is flying.
+     */
     @Override
     public void move() {
-        System.out.println("I really like fly!");
+        System.out.println("I really like to fly!");
     }
 }
