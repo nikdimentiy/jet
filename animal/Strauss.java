fab986178dae737d28951bff6bff879e5d2bdf6c
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-10-25 22:14:51 -0700

    Update Strauss.java

--- animal/Strauss.java
+++ animal/Strauss.java
@@ -1,7 +1,14 @@
+/**
+ * The Strauss class is a subclass of the Bird class and represents a specific type of bird, the Strauss.
+ * Strausses are known for their running abilities.
+ */
 package animal;
 
-public class Strauss extends Bird{
+public class Strauss extends Bird {
+    /**
+     * Overrides the move() method to specify the Strauss's way of moving, which is running.
+     */
     public void move() {
-        System.out.println("I like run!");
+        System.out.println("I like to run!");
     }
 }
