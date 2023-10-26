235fe678e1669648841f698851b6478d157aa92e
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-10-25 22:15:27 -0700

    Update Penguin.java

--- animal/Penguin.java
+++ animal/Penguin.java
@@ -1,6 +1,13 @@
+/**
+ * The Penguin class is a subclass of the Bird class and represents a specific type of bird, the penguin.
+ * Penguins are known for their swimming abilities.
+ */
 package animal;
 
 public class Penguin extends Bird {
+    /**
+     * Overrides the move() method to specify the penguin's way of moving, which is swimming.
+     */
     @Override
     public void move() {
         System.out.println("I can swim!");
