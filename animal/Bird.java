8b518cd7550b5878605c9b96b7e66a2fb5306841
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-10-25 22:16:45 -0700

    Update Bird.java

--- animal/Bird.java
+++ animal/Bird.java
@@ -1,6 +1,13 @@
+/**
+ * The Bird class is a base class that represents a generic bird. It serves as a template for more specific bird types.
+ * The move() method in this class is a placeholder for defining various movement behaviors in subclasses.
+ */
 package animal;
 
 public class Bird {
+    /**
+     * Defines a default method for bird movement, which can be overridden in subclasses.
+     */
     public void move() {
         System.out.println("Different ways");
     }
