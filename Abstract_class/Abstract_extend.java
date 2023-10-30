baf8a450be670cb6e9bb33e7ef3bc9b0525ede31
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-10-29 21:43:16 -0700

    Update Abstract_extend.java

--- Abstract_class/Abstract_extend.java
+++ Abstract_class/Abstract_extend.java
@@ -1,14 +1,18 @@
+/**
+ * This program demonstrates the use of abstract classes and their subclasses.
+ */
 
+// An abstract class AbstractClassA with an abstract method operationA.
 abstract class AbstractClassA {
     public abstract void operationA();
 }
 
-
+// An abstract class AbstractClassB that extends AbstractClassA and adds an abstract method operationB.
 abstract class AbstractClassB extends AbstractClassA {
     public abstract void operationB();
 }
 
-
+// A concrete class ConcreteClass that extends AbstractClassB and provides implementations for both operationA and operationB.
 class ConcreteClass extends AbstractClassB {
     @Override
     public void operationA() {
@@ -23,9 +27,10 @@ class ConcreteClass extends AbstractClassB {
 
 public class Abstract_extend {
     public static void main(String[] args) {
+        // Create an instance of ConcreteClass and assign it to a reference of AbstractClassA.
         AbstractClassA instance = new ConcreteClass();
 
+        // Call the operationA method on the instance.
         instance.operationA();
-
     }
 }
