d9e3c3279bb7ef179b39db31825dd759e3895d98
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-11-22 19:10:48 -0800

    Update Authentification.java

--- Authentification.java
+++ Authentification.java
@@ -1,31 +1,39 @@
-// This tiny code - simplest way to authenticate
-
 import java.util.Scanner;
 
-public class Authentification {
+public class Authentication {
+
     public static void main(String[] args) {
-        String login = "Admin";
-        String password = "P@ssw0rd";
-        // create an object of the Scanner class to handle keyboard input
+        // Define the correct login and password credentials
+        String validLogin = "Admin";
+        String validPassword = "P@ssw0rd";
+
+        // Create a Scanner object to handle user input
         Scanner sc = new Scanner(System.in);
 
+        // Prompt the user to enter their login
         System.out.print("Enter login, please: ");
 
-        // create a string local variable named "userLogin"
-        // and accept input from the user into it
+        // Capture the user's login input
+        String enteredLogin = sc.next();
 
-        String usersLogin = sc.next();
-
-        if (login.equals(usersLogin)) {
+        // Check if the entered login matches the valid login
+        if (validLogin.equals(enteredLogin)) {
+            // Prompt the user to enter their password
             System.out.print("Enter password, please: ");
-            String usersPassword = sc.next();
 
-            if (password.equals(usersPassword)) {
-                System.out.println("Welcome " + usersLogin + ", you enter in the system!");
+            // Capture the user's password input
+            String enteredPassword = sc.next();
+
+            // Check if the entered password matches the valid password
+            if (validPassword.equals(enteredPassword)) {
+                // Welcome the user if they entered correct credentials
+                System.out.println("Welcome " + enteredLogin + ", you have successfully entered the system!");
             } else {
-                System.out.println("You entered an incorrect password! ");
+                // Inform the user if they entered an incorrect password
+                System.out.println("You entered an incorrect password!");
             }
         } else {
+            // Inform the user if they entered an invalid login
             System.out.println("There is no user with that name!");
         }
     }
