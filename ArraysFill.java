be8ac737a62095b2a2d7ea63a3071b281509bc90
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-11-25 22:42:09 -0800

    Update ArraysFill.java
    adding comments

--- ArraysFill.java
+++ ArraysFill.java
@@ -1,13 +1,20 @@
-// Filling in an array and displaying values on the screen
-
+/**
+ * This program demonstrates the creation of an array, filling it with values,
+ * and then displaying the values on the screen.
+ */
 public class ArraysFill {
 
+    /**
+     * The main method serves as the entry point of the program.
+     *
+     * @param args The command-line arguments (not used in this example).
+     */
     public static void main(String[] args) {
-        // creating an array of integer elements
-        // and filling it with the values 1,2,3,4,5
+        // Creating an array of integer elements and filling it with the values 1, 2, 3, 4, 5
         int[] array = new int[]{1, 2, 3, 4, 5};
 
-        // Displays the values of the array elements
+        // Displaying the values of the array elements
+        System.out.println("Array Elements:");
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
         }
