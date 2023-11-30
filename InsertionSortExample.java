a8997b36920adaf825cca822f75493297708b77b
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-11-29 23:34:31 -0800

    Update InsertionSortExample.java

--- InsertionSortExample.java
+++ InsertionSortExample.java
@@ -1,25 +1,34 @@
-/* Insertion sort is a simple sorting algorithm that 
-   builds the final sorted array one item at a time. 
-   It is much less efficient on large lists than more advanced algorithms
-   such as quicksort, heapsort, or merge sort.
-*/
-
+/**
+ * InsertionSortExample is a class that demonstrates the insertion sort algorithm.
+ * Insertion sort builds the final sorted array one element at a time, making it less
+ * efficient on large lists compared to more advanced algorithms like quicksort, heapsort, or merge sort.
+ */
 public class InsertionSortExample {
 
+    /**
+     * The main method is the entry point of the program.
+     *
+     * @param args Command line arguments (not used in this example).
+     */
     public static void main(String a[]) {
-        int[] myArray = { 860, 8, 200, 9 };
+        int[] myArray = {860, 8, 200, 9};
 
         System.out.println("Before Insertion Sort");
 
         printArray(myArray);
 
-        insertionSort(myArray);// sorting array using insertion sort
+        insertionSort(myArray); // Sorting array using insertion sort
 
         System.out.println("After Insertion Sort");
 
         printArray(myArray);
     }
 
+    /**
+     * This method implements the insertion sort algorithm to sort an array of integers in ascending order.
+     *
+     * @param arr The array to be sorted.
+     */
     public static void insertionSort(int arr[]) {
         int n = arr.length;
 
@@ -39,6 +48,11 @@ public class InsertionSortExample {
         }
     }
 
+    /**
+     * This method prints the elements of an array.
+     *
+     * @param array The array to be printed.
+     */
     static void printArray(int[] array) {
 
         for (int i = 0; i < array.length; i++) {
