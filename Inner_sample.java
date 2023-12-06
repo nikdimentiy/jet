7e85910ca85c5c3fc2e3d134574bc8541dd9544f
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-12-05 23:18:56 -0800

    Update Inner_sample.java
    add comments

--- Inner_sample.java
+++ Inner_sample.java
@@ -1,17 +1,27 @@
-// sample of Inner class with call method
-
+// Outer class definition
 class MyClass {
+    
+    // Inner class definition
     public class Inner {
+        
+        /**
+         * This method prints a greeting message.
+         */
         public void method() {
             System.out.println("Hello!");
         }
     }
 }
 
+// Main class definition
 public class Main {
 
     public static void main(String[] args) {
+        
+        // Create an instance of the Inner class using the outer class instance
         MyClass.Inner instance = new MyClass().new Inner();
+        
+        // Call the method of the Inner class
         instance.method();
     }
 }
