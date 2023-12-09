bf418777595e6ebb69f96806696822770dca206c
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-12-08 20:56:19 -0800

    Update LogicShift.java

--- LogicShift.java
+++ LogicShift.java
@@ -1,19 +1,24 @@
-// logic shift
+/**
+ * This program demonstrates logical left and right shifts on a byte value.
+ */
 public class LogicShift {
 
+    /**
+     * Main method to execute the logic shift operations.
+     *
+     * @param args The command-line arguments (not used in this program).
+     */
     public static void main(String[] args) {
-        byte operand = 0b0000001;              // 0000 0001
-        System.out.println("Number before shift : " + operand);
+        // Initialize a byte variable with binary value 0000 0001
+        byte operand = 0b0000001;
+        System.out.println("Number before shift: " + operand);
 
-        // Logically shift a number to the left
+        // Perform a logical left shift by 2 positions (resulting in 0000 0100)
+        operand = (byte) (operand << 2);
+        System.out.println("Number after left shift: " + operand);
 
-        operand = (byte) (operand << 2);   // 0000 0100
-        System.out.println("Number after left shift : " + operand);
-
-        // Logically shift a number to the right
-
-        operand = (byte) (operand >> 1);   // 0000 0010
-
+        // Perform a logical right shift by 1 position (resulting in 0000 0010)
+        operand = (byte) (operand >>> 1);
         System.out.println("Number after right shift: " + operand);
     }
 }
