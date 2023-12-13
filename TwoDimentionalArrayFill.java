fba87d201fbc24e102d1acdc4d12613fdccba239
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-12-12 22:56:30 -0800

    Update TwoDimentionalArrayFill.java

--- TwoDimentionalArrayFill.java
+++ TwoDimentionalArrayFill.java
@@ -1,18 +1,27 @@
-// using a cycle (for-each) to sum up values in a two-dimensional array
+/**
+ * The TwoDimentionalArrayFill class demonstrates the use of a two-dimensional array
+ * and a nested loop to fill and print values in the array.
+ */
 public class TwoDimentionalArrayFill {
 
+    /**
+     * The main method is the entry point of the program.
+     *
+     * @param args The command-line arguments (unused in this example).
+     */
     public static void main(String[] args) {
-        int sizeI = 3;
-        int sizeJ = 3;
+        int sizeI = 3; // Number of rows in the 2D array
+        int sizeJ = 3; // Number of columns in the 2D array
         int[][] array = new int[sizeI][sizeJ];
 
-        // Array fill
+        // Array fill using nested loops
         for (int i = 0; i < sizeI; i++) {
             for (int j = 0; j < sizeJ; j++) {
                 array[i][j] = i * j + 1;
             }
         }
 
+        // Printing the filled 2D array
         for (int i = 0; i < sizeI; i++) {
             for (int j = 0; j < sizeJ; j++) {
                 System.out.print(" " + array[i][j]);
