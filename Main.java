32076269e359abe007fa944d3038420886ac9678
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-12-12 22:54:09 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,30 @@
+/**
+ * The Main class demonstrates the usage of the Car class to set and retrieve
+ * the maximum speed of two cars.
+ */
 public class Main {
+
+    /**
+     * The main method is the entry point of the program.
+     *
+     * @param args The command-line arguments (unused in this example).
+     */
     public static void main(String[] args) {
+        // Creating two Car objects
         Car car1 = new Car();
         Car car2 = new Car();
 
+        // Setting the maximum speed of car1 to 150
         car1.setMaxSpeed(150);
+        // Retrieving the maximum speed of car1
         int speed = car1.getMaxSpeed();
-        System.out.println("Maximus speed of car ONE is: " + speed);
+        // Printing the maximum speed of car1
+        System.out.println("Maximum speed of car ONE is: " + speed);
 
+        // Setting the maximum speed of car2 to 200
         car2.setMaxSpeed(200);
-        System.out.print("Maximus speed of car TWO is: ");
+        // Printing the maximum speed of car2 using separate print statements
+        System.out.print("Maximum speed of car TWO is: ");
         System.out.println(car2.getMaxSpeed());
     }
 }
