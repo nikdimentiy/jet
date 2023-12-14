89fc4733cd8ae248ec9c8e8847363578c44d38a9
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-12-13 23:17:42 -0800

    Update TwoDimenArrayFillMatrix.java

--- TwoDimenArrayFillMatrix.java
+++ TwoDimenArrayFillMatrix.java
@@ -1,19 +1,31 @@
-// Two-dimensional arrays fill with matrix look
+/**
+ * TwoDimenArrayFillMatrix class demonstrates the initialization and printing
+ * of a two-dimensional array with a matrix look.
+ */
 public class TwoDimenArrayFillMatrix {
+
+    /**
+     * The main method initializes a 3x3 matrix and prints its elements.
+     *
+     * @param args The command-line arguments (unused).
+     */
     public static void main(String[] args) {
-        // The compiler determines the size of the array
-        // based on the initialization expression
+        // Initialize a 3x3 matrix
         int[][] array = {
                 {1, 2, 3},
                 {4, 5, 6},
                 {7, 8, 9}
         };
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        // Iterate through rows
+        for (int i = 0; i < array.length; i++) {
+            // Iterate through columns
+            for (int j = 0; j < array[i].length; j++) {
+                // Print each element with a space
                 System.out.print(" " + array[i][j]);
             }
-            System.out.print("\n");
+            // Move to the next line after printing a row
+            System.out.println();
         }
     }
 }
