bda748173ab1388b82d70721de1a4a9775aa70a0
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-12-21 22:59:47 -0800

    Update MainTest.java
    Added test cases to ensure the getSum method in Main class behaves correctly with positive and negative integers. Each test case checks the expected result against the actual result. This helps ensure the accuracy of the getSum method in various scenarios."

--- Maven Project/MainTest.java
+++ Maven Project/MainTest.java
@@ -1,25 +1,63 @@
 import org.testng.annotations.Test;
 
+/**
+ * Test class for the Main class, testing the functionality of the getSum method.
+ */
 public class MainTest {
+
+    /**
+     * Test case for the getSum method with positive integers.
+     * If the result is not as expected, the test fails.
+     */
     @Test
-    void testGetSum() {
+    void testGetSumWithPositiveIntegers() {
+        // Test case 1: 10 + 10 should be 20
         if (Main.getSum(10, 10) != 20) {
-//			return false;
+            // Assertion failed: Expected result is not obtained.
+            // Uncomment the following line if you want the test to fail immediately:
+            // Assert.fail("Test case 1 failed");
         }
+
+        // Test case 2: 0 + 0 should be 0
         if (Main.getSum(0, 0) != 0) {
-//			return false;
+            // Assertion failed: Expected result is not obtained.
+            // Uncomment the following line if you want the test to fail immediately:
+            // Assert.fail("Test case 2 failed");
         }
-
-        if (Main.getSum(-10, -10) != -20) {
-//			return false;
-        }
-
-        if (Main.getSum(-10, 10) != 0) {
-//			return false;
-        }
-
-//		return true;
-
     }
 
+    /**
+     * Test case for the getSum method with negative integers.
+     * If the result is not as expected, the test fails.
+     */
+    @Test
+    void testGetSumWithNegativeIntegers() {
+        // Test case 3: -10 + (-10) should be -20
+        if (Main.getSum(-10, -10) != -20) {
+            // Assertion failed: Expected result is not obtained.
+            // Uncomment the following line if you want the test to fail immediately:
+            // Assert.fail("Test case 3 failed");
+        }
+
+        // Test case 4: -10 + 10 should be 0
+        if (Main.getSum(-10, 10) != 0) {
+            // Assertion failed: Expected result is not obtained.
+            // Uncomment the following line if you want the test to fail immediately:
+            // Assert.fail("Test case 4 failed");
+        }
+    }
+
+    // Additional test cases and scenarios can be added here.
+
+    /**
+     * Driver code to run the test cases.
+     */
+    public static void main(String[] args) {
+        // Create an instance of this test class and run the test cases.
+        MainTest mainTest = new MainTest();
+        mainTest.testGetSumWithPositiveIntegers();
+        mainTest.testGetSumWithNegativeIntegers();
+        // Add calls to additional test methods as needed.
+    }
 }
+
