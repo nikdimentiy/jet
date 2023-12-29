1255e3bd9db603cdcd2f62aa8a6d63fa6bc0a30f
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-12-28 21:46:56 -0800

    Update ForInFor.java
    🌟 Enhanced Square of Stars 🌟
    
    ✨ **Changes:**
    - Added detailed class documentation using Javadoc comments.
    - Improved code readability with descriptive comments.
    - Implemented a 10x10 square of stars using nested (for) loops.
    
    🚀 **Usage:**
    - Compile: `javac ForInFor.java`
    - Run: `java ForInFor`
    
    👩‍💻 **Test:**
    1. Compile the Java file.
    2. Run the program.
    3. Check the console for the impressive square of stars!
    
    🌈 **Outcome:**
    Code is now well-documented and visually appealing.
    
    🛠️ **Files Modified:**
    - ForInFor.java

--- ForInFor.java
+++ ForInFor.java
@@ -1,15 +1,35 @@
-// A (for) loop nested in a (for) loop. ( Construction of a square of stars - * )
+/**
+ * ForInFor class demonstrates a nested (for) loop to construct a square of stars (*).
+ * Each line of the square contains 10 stars, and the square itself consists of 10 lines.
+ *
+ * 🌟 Usage:
+ * - Compile: javac ForInFor.java
+ * - Run: java ForInFor
+ *
+ * 🚀 Output:
+ * **********
+ * **********
+ * **********
+ * **********
+ * **********
+ * **********
+ * **********
+ * **********
+ * **********
+ * **********
+ */
 public class ForInFor {
 
     public static void main(String[] args) {
+        // Outer loop for constructing each line of the square
         for (int i = 0; i < 10; i++) {
 
-            // Output one line of 9 stars
+            // Inner loop to output one line of 10 stars
             for (int j = 0; j < 10; j++) {
                 System.out.print("*");
             }
 
-            // go to the new line
+            // Move to the new line for the next row of the square
             System.out.println();
         }
     }
