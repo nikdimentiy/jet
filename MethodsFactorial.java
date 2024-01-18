3159bf51804ca52ea52019123b7c57a9351e53fc
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-01-17 22:06:18 -0800

    Update MethodsFactorial.java
    Add Java code for calculating factorial
    
    This commit introduces a Java class named MethodsFactorial, demonstrating the calculation of the factorial of a number using recursion. The `factorial` method is implemented with a recursive approach, and the main method showcases its usage by calculating and printing the factorial of 10.

--- MethodsFactorial.java
+++ MethodsFactorial.java
@@ -1,16 +1,31 @@
-// Factorial of number
+/**
+ * This class demonstrates the calculation of the factorial of a number using recursion.
+ */
 public class MethodsFactorial {
-// we using recursion method
+
+    /**
+     * Recursive method to calculate the factorial of a number.
+     * @param n The number for which the factorial is to be calculated.
+     * @return The factorial of the given number.
+     */
     static int factorial(int n) {
+        // Base case: factorial of 0 is 1
         if (n == 0)
             return 1;
+        // Recursive case: n! = n * (n-1)!
         else
             return n * factorial(n - 1);
     }
 
+    /**
+     * Main method to demonstrate the usage of the factorial method.
+     * @param args Command-line arguments (not used in this example).
+     */
     public static void main(String[] args) {
-        int factorial = factorial(10);
+        // Calculate the factorial of 10
+        int factorialResult = factorial(10);
 
-        System.out.println(factorial);
+        // Print the result
+        System.out.println(factorialResult);
     }
 }
