5bd78920743a02948110720a67bdf9bb059628af
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-01-19 23:20:42 -0800

    Update LogWithTernary.java
    Add Java program demonstrating the use of ternary operator in authentication
    
    This commit adds a small Java program, LogWithTernary.java, that showcases the usage of the ternary operator in the first step of authentication. The program takes user input for login and uses the ternary operator to determine the role (Administrator or User), which is then concatenated to a greeting string.

--- LogWithTernary.java
+++ LogWithTernary.java
@@ -1,21 +1,29 @@
-// This small code introduces how to use the ternary operator in first step of authentication
+// This Java program demonstrates the use of the ternary operator in the first step of authentication.
 
 import java.util.Scanner;
 
 public class LogWithTernary {
-    // a ternary conditional operation
+    // Declaring a class named LogWithTernary
 
+    // The main method, where the execution of the program begins
     public static void main(String[] args) {
+        // Declaring a String variable named "string" and initializing it with "Hello "
         String string = "Hello ";
 
+        // Printing a message to the console asking the user to enter their login
         System.out.println("Enter your login: ");
+
+        // Creating a Scanner object to take input from the user
         Scanner in = new Scanner(System.in);
 
-        // create a variable named "login" of the String type and accept input from the user into it
+        // Creating a variable named "login" of the String type and accepting input from the user into it
         String login = in.next();
 
-        // variable "string" assign the return value of the ternary operator
+        // Using the ternary operator to determine the role and concatenate it to the "string" variable
+        // If login equals "Admin", append "Administrator" to "string", otherwise append "User"
         string += login.equals("Admin") ? "Administrator" : "User";
+
+        // Printing the final result stored in the "string" variable to the console
         System.out.println(string);
     }
 }
