4455182be354f038ecb245a5375f504540c44e61
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-02-01 22:53:17 -0800

    Add binary conversion functionality
    Add binary conversion functionality
    
    - Modified Program class to take user input for an integer
    - Implemented toBinary method in Converter class for binary conversion
    - Updated main method to display binary representation of the entered integer

--- binary_convertation.java
+++ binary_convertation.java
@@ -3,20 +3,44 @@ package practice;
 import java.util.Scanner;
 
 public class Program {
+    /**
+     * This is the main method of the program.
+     * It takes an integer input, converts it to binary using the Converter class,
+     * and prints the result.
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // Prompt the user to enter an integer
+        System.out.print("Enter an integer: ");
         int x = sc.nextInt();
+
+        // Print the binary representation of the entered integer
         System.out.print(Converter.toBinary(x));
     }
 }
 
 class Converter {
+    /**
+     * Converts an integer to its binary representation.
+     *
+     * @param number The integer to be converted to binary.
+     * @return The binary representation of the input integer.
+     */
     public static String toBinary(int number) {
+        // StringBuilder to store the binary representation
         StringBuilder binary = new StringBuilder();
+
+        // Loop until the input number becomes 0
         while (number > 0) {
+            // Insert the remainder (either 0 or 1) at the beginning of the binary string
             binary.insert(0, (number % 2));
+
+            // Divide the number by 2 to move to the next bit
             number /= 2;
         }
+
+        // Return the binary representation as a string
         return binary.toString();
     }
 }
