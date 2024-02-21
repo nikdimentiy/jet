f0b5b3615a15eb2e765b575ea6fdd494be0769d2
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-02-20 21:10:34 -0800

    Add Java program for Hailstone numbers generation
    Added a Java program that generates Hailstone numbers for a given integer and calculates the number of steps required to reduce it to 1.

--- HailstoneNumbersOut.java
+++ HailstoneNumbersOut.java
@@ -1,48 +1,59 @@
-// // Java program to generate hailstone
-// numbers and calculate steps required
-// to reduce them to 1
-
 import java.util.*;
 
+/**
+ * The HailstoneNumbersOut class generates hailstone numbers for a given integer and
+ * calculates the number of steps required to reduce the number to 1.
+ */
 class HailstoneNumbersOut {
     static int c;
 
-    // function to print hailstone numbers
-    // and to calculate the number of steps
-    // required
+    /**
+     * Recursively generates hailstone numbers for the given integer and prints them.
+     * Also calculates the number of steps required to reduce the number to 1.
+     *
+     * @param N The input integer for which hailstone numbers are generated.
+     * @return The number of steps required to reduce the input integer to 1.
+     */
     static int HailstoneNumbers(int N) {
+        // Print the current hailstone number.
         System.out.print(N + " ");
 
+        // Base case: N is initially 1.
         if (N == 1 && c == 0) {
-
-            // N is initially 1.
             return c;
-        } else if (N == 1 && c != 0) {
-
-            // N is reduced to 1.
+        }
+        // Base case: N is reduced to 1.
+        else if (N == 1 && c != 0) {
             c++;
             return c;
-        } else if (N % 2 == 0) {
-
-            // If N is Even.
+        }
+        // If N is Even.
+        else if (N % 2 == 0) {
             c++;
+            // Recursively call HailstoneNumbers with N/2.
             HailstoneNumbers(N / 2);
-        } else if (N % 2 != 0) {
-
-            // N is Odd.
+        }
+        // If N is Odd.
+        else if (N % 2 != 0) {
             c++;
+            // Recursively call HailstoneNumbers with 3*N + 1.
             HailstoneNumbers(3 * N + 1);
         }
         return c;
     }
 
-    // Driver code
+    /**
+     * The main method to demonstrate the generation of hailstone numbers and
+     * display the number of steps required to reduce a given integer to 1.
+     *
+     * @param args The command line arguments (not used in this program).
+     */
     public static void main(String[] args) {
+        // Initial value for the hailstone sequence.
         int N = 7;
         int x;
 
-        // Function to generate Hailstone
-        // Numbers
+        // Generate Hailstone Numbers and get the number of steps.
         x = HailstoneNumbers(N);
 
         // Output: Number of Steps
@@ -50,5 +61,3 @@ class HailstoneNumbersOut {
         System.out.println("Number of Steps: " + x);
     }
 }
-
-
