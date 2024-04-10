134a29452de7ba683ef8e02761a5d02f3d7b272e
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-04-09 21:27:19 -0700

    Added code for finding the minimum value in an array with improved modularity and comments

--- MinArray.java
+++ MinArray.java
@@ -1,16 +1,34 @@
-// Finding a minimum in an array
-
+/**
+ * This class demonstrates finding the minimum value in an array of integers.
+ */
 public class MinArray {
-    public static void main(String[] args) {
 
-        int[] list = {5, 6, 7, -7, -9, 2, 0, 11, -35, 5};
+    /**
+     * Finds the minimum value in the given array of integers.
+     * @param list an array of integers
+     * @return the minimum value found in the array
+     */
+    static int findMin(int[] list) {
+        int min = list[0]; // Assume the first element as the minimum
 
-        int min = list[0];
-
+        // Iterate through the array to find the minimum value
         for (int i = 1; i < list.length; i++) {
             if (list[i] < min)
                 min = list[i];
         }
+        return min; // Return the minimum value
+    }
+
+    /**
+     * The main method to execute the demonstration.
+     * @param args command-line arguments (not used in this demonstration)
+     */
+    public static void main(String[] args) {
+        int[] list = {5, 6, 7, -7, -9, 2, 0, 11, -35, 5};
+        
+        // Call the findMin method to get the minimum value
+        int min = findMin(list);
+
         System.out.println("Min is " + min);
     }
 }
