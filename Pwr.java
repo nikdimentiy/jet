ff16a9d5ff2d6b7eb54fe82d4c68bd73bda011a6
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-04-17 23:45:48 -0700

    Added Pwr class for power calculation with documentation and code improvements.

--- Pwr.java
+++ Pwr.java
@@ -1,17 +1,43 @@
+/**
+ * The Pwr class represents a simple power calculation utility.
+ * It calculates the power of a given base to a given exponent.
+ */
 public class Pwr {
-    double b;
-    int e;
-    double val;
+    // Instance variables
+    private double base;
+    private int exponent;
+    private double value; // The result of the power calculation
 
-    Pwr(double base, int exp) {
-        this.b = base;
-        this.e = exp;
+    /**
+     * Constructs a Pwr object with the specified base and exponent.
+     *
+     * @param base     the base of the power calculation
+     * @param exponent the exponent of the power calculation
+     */
+    public Pwr(double base, int exponent) {
+        this.base = base;
+        this.exponent = exponent;
 
-        val = 1;
-        if(exp == 0) return;
-        for (; exp > 0; exp--) val = val * base;
+        // Initialize the value to 1, as any number to the power of 0 is 1
+        value = 1;
+
+        // Perform the power calculation
+        if (exponent == 0) {
+            // If the exponent is 0, the result is always 1
+            return;
+        }
+        for (; exponent > 0; exponent--) {
+            // Multiply the base by itself 'exponent' times
+            value *= base;
+        }
     }
-    double get_pwr() {
-        return val;
+
+    /**
+     * Returns the calculated power value.
+     *
+     * @return the result of the power calculation
+     */
+    public double getPower() {
+        return value;
     }
 }
