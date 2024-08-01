1cc51279804b610b8d7dbe7b7d98d2f48c6cce29
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-07-31 22:56:09 -0700

    This Java code demonstrates a simple class hierarchy with two classes: A and B. Class A prints a message when instantiated, while class B, which extends A, also prints a message upon creation.

--- super_contstructor.java
+++ super_contstructor.java
@@ -1,16 +1,41 @@
+/**
+ * Class A represents a base class that prints a message when an instance is created.
+ */
 class A {
+    /**
+     * Constructor for class A.
+     * Prints a message indicating that a new instance of A has been created.
+     */
     public A() {
         System.out.println("New A");
     }
 }
+
+/**
+ * Class B is a subclass of A that prints a message when an instance is created.
+ */
 class B extends A {
+    /**
+     * Constructor for class B.
+     * Calls the constructor of class A and then prints a message indicating that a new instance of B has been created.
+     */
     public B() {
+        super(); // Explicitly calling the constructor of the superclass A
         System.out.println("New B");
     }
 }
 
+/**
+ * The Program class contains the main method to run the application.
+ */
 class Program {
-    public static void main(String[ ] args) {
-        B obj = new B();
+    /**
+     * The main method is the entry point of the application.
+     * It creates an instance of class B, which will also create an instance of class A.
+     *
+     * @param args Command line arguments (not used in this program).
+     */
+    public static void main(String[] args) {
+        B obj = new B(); // Creating an instance of B, which triggers the constructor chain
     }
 }
